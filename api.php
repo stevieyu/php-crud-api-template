4514f65d50281aab6b9dc8633b24238771a787c3
Stevie <yzm629@sina.cn>
2021-02-20 16:33:38 +0800

    feat: update

--- api.php
+++ api.php
@@ -4,21 +4,20 @@ namespace Tqdev\PhpCrudUi;
 
 use Tqdev\PhpCrudApi\RequestFactory;
 use Tqdev\PhpCrudApi\ResponseUtils;
-use Tqdev\PhpCrudUi\Config;
-use Tqdev\PhpCrudUi\Ui;
+use Tqdev\PhpCrudApi\Config;
+use Tqdev\PhpCrudApi\Api;
 
-require '../vendor/autoload.php';
+//require './vendor/autoload.php';
+require './vendor.phar';
 
 $config = new Config([
-    'api' => [
-        'address' => 'sdm688990573.my3w.com',
-        'username' => 'sdm688990573',
-        'password' => 'asAS12!@',
-        'database' => 'sdm688990573_db',
-        'basePath' => '/api',
-    ]
+    'address' => 'sdm688990573.my3w.com',
+    'username' => 'sdm688990573',
+    'password' => 'asAS12!@',
+    'database' => 'sdm688990573_db',
+    'basePath' => '/api',
 ]);
 $request = RequestFactory::fromGlobals();
-$ui = new Ui($config);
+$ui = new Api($config);
 $response = $ui->handle($request);
 ResponseUtils::output($response);
