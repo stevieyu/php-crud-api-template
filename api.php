3f734d15fb6c940933916ecfb57011909a79a562
Stevie <yzm629@sina.cn>
2022-08-06 11:58:36 +0800

    feat: default display error

--- api.php
+++ api.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR);
+
 require file_exists('./vendor/autoload.php') ? 'vendor/autoload.php' : 'vendor.phar';
 
 use Tqdev\PhpCrudApi\RequestFactory;
