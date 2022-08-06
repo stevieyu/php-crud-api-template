a5df72e4294580227761d7554e212a83779efea0
Stevie <yzm629@sina.cn>
2022-08-06 12:28:31 +0800

    feat: default display error

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ERROR);
 
 require file_exists('./vendor/autoload.php') ? 'vendor/autoload.php' : 'vendor.phar';
@@ -19,15 +19,15 @@ if ($request_url === '/api/docs') {
 }
 
 $config = new Config([
-    'address' => getenv('DB_HOST') ?: 'sdm723416260.my3w.com',
-    'port' => getenv('DB_PORT') ?: '3306',
-    'username' => getenv('DB_USERNAME') ?: 'sdm723416260',
-    'password' => getenv('DB_PASSWORD') ?: 'QKW7e5YE',
-    'database' => getenv('DB_NAME') ?: 'sdm723416260_db',
+    'address' => getenv('DB_HOST') ?: $_ENV['DB_HOST'] ?? 'sdm723416260.my3w.com',
+    'port' => getenv('DB_PORT') ?: $_ENV['DB_PORT'] ?? '3306',
+    'username' => getenv('DB_USERNAME') ?: $_ENV['DB_USERNAME'] ?? 'sdm723416260',
+    'password' => getenv('DB_PASSWORD') ?: $_ENV['DB_PASSWORD'] ?? 'QKW7e5YE',
+    'database' => getenv('DB_NAME') ?: $_ENV['DB_NAME'] ?? 'sdm723416260_db',
     'basePath' => '/api',
     'cacheTime' => 1,
 //    'cachePath' => __DIR__.'/.cache',
-    'cachePath' => '/tmp',
+    'cachePath' => getenv('CACHE_PATH') ?: $_ENV['CACHE_PATH'] ?? '/tmp',
     'middlewares' => 'cors,errors,json',
     'json.tables' => 'pages',
     'json.columns' => 'data',
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-require_once './api.php';
+
+$_ENV['DB_HOST'] = '127.0.0.1';
+$_ENV['DB_USERNAME'] = 'tx9z7gn';
+$_ENV['DB_PASSWORD'] = 'hgUS7O';
+$_ENV['DB_NAME'] = 'tx9z7gn';
+
+$_ENV['CACHE_PATH'] = __DIR__.'/.cache';
+
+require_once 'output/api.phar';
 
